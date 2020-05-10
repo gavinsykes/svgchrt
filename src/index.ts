9033e11ec2a6cc163ee486974e97e658fe2bc3ef
Gavin Sykes <gavin@gavinsykes.uk>
2020-05-10 17:18:19 +0100

    Completed isObject.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import defaultSettings from './defaultSettings';
+import isObject from './isObject';
+import deepObjectMerge from './deepObjectMerge';
 
-console.log(defaultSettings.defaultSettings.title);
+export function SVGChrt(data = [], options = {}) {
+  this.defaultSettings = defaultSettings.defaultSettings;
+  this.isObject = isObject.isObject;
+  this.deepObjectMerge = deepObjectMerge.deepObjectMerge;
+}
---
+++ src/isObject.ts
@@ -0,0 +1,5 @@
+const isObject: (o: any) => boolean = function(o) {
+  return o && typeof o === "object" && !Array.isArray(o);
+}
+
+export default { isObject };
