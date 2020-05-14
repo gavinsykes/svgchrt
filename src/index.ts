672ac4faed283b7c70c73a825288f9a71dc040ac
Gavin Sykes <gavin@gavinsykes.uk>
2020-05-14 20:24:57 +0100

    Importing more into index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import defaultSettings from './defaultSettings';
 import isObject from './isObject';
 import deepObjectMerge from './deepObjectMerge';
+import appendSVGChild from './appendSVGChild';
+import svgWrapText from './svgWrapText';
+import updateData from './updateData';
+import updateOptions from './updateOptions';
+import placeCanvas from './placeCanvas';
+import addTitle from './addTitle';
+import addSubtitle from './addSubtitle';
+import addDescription from './addDescription';
+import addLegend from './addLegend';
 
-export function SVGChrt(data = [], options = {}) {
-  this.defaultSettings = defaultSettings.defaultSettings;
-  this.isObject = isObject.isObject;
-  this.deepObjectMerge = deepObjectMerge.deepObjectMerge;
+export function SVGChrt(data = [], options = {}): void {
+  defaultSettings : defaultSettings;
+  isObject        : isObject;
+  deepObjectMerge : deepObjectMerge;
+  appendSVGChild  : appendSVGChild;
+  svgWrapText     : svgWrapText;
+  updateData      : updateData;
+  updateOptions   : updateOptions;
+  placeCanvas     : placeCanvas;
+  addTitle        : addTitle;
+  addSubtitle     : addSubtitle;
 }
