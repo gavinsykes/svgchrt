289706b7d713c8a8fac4821067a2d7b60965a283
Gavin Sykes <gavin@gavinsykes.uk>
2020-05-14 20:20:03 +0100

    Refactoring default settings

--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -1,4 +1,73 @@
-const defaultSettings = {
+interface PaddingandMargin {
+  top    : number;
+  right  : number;
+  bottom : number;
+  left   : number;
+}
+
+interface LegendItemIcon {
+  cx?     : string;
+  cy?     : string;
+  d?      : string;
+  display : boolean;
+  height? : number;
+  r?      : string;
+  rx?     : string;
+  ry?     : string;
+  shape   : string;
+  width?  : number;
+}
+
+interface LegendItem {
+  class?       : string;
+  displayName  : string;
+  icon?        : LegendItemIcon;
+}
+
+interface Legend {
+  background    : object;
+  displaceTitle : boolean;
+  display       : boolean;
+  icons         : LegendItemIcon;
+  itemMargin    : PaddingandMargin;
+  layOverChart  : boolean;
+  margin        : PaddingandMargin;
+  orientation   : string;
+  padding       : PaddingandMargin;
+  position      : string;
+  title         : string;
+}
+
+interface Canvas {
+  height  : number;
+  padding : PaddingandMargin;
+  viewBox : string;
+  width   : number;
+}
+
+interface Chart {
+  margin : PaddingandMargin;
+}
+
+interface Titles {
+  display : boolean;
+  margin  : PaddingandMargin;
+  text    : string;
+}
+
+export interface SettingsObject {
+  background  : boolean;
+  canvas      : Canvas;
+  chart       : Chart;
+  description : string;
+  id          : string;
+  legend      : Legend;
+  subtitle    : Titles;
+  target      : string | undefined;
+  title       : Titles;
+}
+
+const defaultSettings: SettingsObject = {
   background  : true,
   canvas      : {
     height  : 500,
@@ -10,7 +79,7 @@ const defaultSettings = {
     },
     viewBox  : '0 0 960 500',
     width    : 960
-  },
+  } as Canvas,
   chart       : {
     margin : {
       top    : 10,
@@ -18,12 +87,16 @@ const defaultSettings = {
       bottom : 10,
       left   : 10
     }
-  },
+  } as Chart,
   description : '',
   id          : '',
   legend      : {
     background    : {
-      display : false
+      color   : 'white',
+      display : false,
+      r       : 0,
+      rx      : 0,
+      ry      : 0
     },
     displaceTitle : false,
     display       : false,
@@ -32,14 +105,14 @@ const defaultSettings = {
       height  : 14,
       shape   : 'rect',
       width   : 14
-    },
+    } as LegendItemIcon,
     itemMargin    : {
       top    : 5,
       right  : 5,
       bottom : 5,
       left   : 5
     },
-    items         : [],
+    items         : [] as LegendItem[],
     layOverChart  : false,
     margin        : {
       top    : 10,
@@ -56,7 +129,7 @@ const defaultSettings = {
     },
     position      : 'right',
     title         : ''
-  },
+  } as Legend,
   subtitle    : {
     display : false,
     margin  : {
@@ -80,4 +153,4 @@ const defaultSettings = {
   }
 };
 
-export default { defaultSettings };
+export default defaultSettings;
