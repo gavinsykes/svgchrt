2ece88fd1bb4077260706c08cabebce31a748a07
Gavin Sykes <gavin@gavinsykes.uk>
2020-05-14 20:19:32 +0100

    Adding description function

---
+++ src/addDescription.ts
@@ -0,0 +1,10 @@
+import appendSVGChild from './appendSVGChild';
+import defaultSettings from './defaultSettings';
+
+function addDescription(settings = defaultSettings, canvas: SVGElement) {
+  if (settings.description) {
+    appendSVGChild('desc', canvas, {}, settings.description) as SVGDescElement;
+  }
+}
+  
+export default addDescription;
