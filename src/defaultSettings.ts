e05f062a7a683b3692a679f63a2339bd2d78cb63
Gavin Sykes <gavin@gavinsykes.uk>
2020-05-14 21:08:02 +0100

    Function isn't callable apparently, ain't nobody got time for this.

--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -20,7 +20,7 @@ export interface LegendItemIcon {
 
 interface LegendItem {
   class?       : string;
-  displayName  : string | Function;
+  displayName  : string;
   icon?        : LegendItemIcon;
 }
 
