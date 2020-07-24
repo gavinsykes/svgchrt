f6587cd36b95a2d710e7b5812f09a9363d2fc133
Gavin Sykes <gavin@gavinsykes.uk>
2020-07-24 23:49:03 +0100

    Copying the original object when merging, rather than mutating the original

--- src/deepObjectMerge.ts
+++ src/deepObjectMerge.ts
@@ -2,9 +2,15 @@ import isObject from './isObject';
 import { LegendItemIcon } from './defaultSettings';
 import { LayoutItem } from './interfaces';
 
+function copyObject(original: any): object {
+  let returnedObject = {};
+  Object.entries(original).map(c => returnedObject[c[0]] = isObject(c[1]) ? copyObject(c[1]) : c[1]);
+  return returnedObject;
+}
+
 function deepObjectMerge (original: any, ...newobjs: any[]): object | LegendItemIcon | LayoutItem {
   if (!newobjs.length) return original;
-  let returnedObj = original;
+  let returnedObj = copyObject(original);
   const newobj: any = newobjs.shift();
   if (isObject(returnedObj) && isObject(newobj)) {
     for (const key in newobj) {
