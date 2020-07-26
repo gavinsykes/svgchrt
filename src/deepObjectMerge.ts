7d9aff37cacaa0221b2ba3ced1f0c11d23ca8edd
Gavin Sykes <gavin@gavinsykes.uk>
2020-07-27 00:31:03 +0100

    Tweaking deepObjectMerge. Again.

--- src/deepObjectMerge.ts
+++ src/deepObjectMerge.ts
@@ -4,19 +4,17 @@ import { LegendItemIcon } from './defaultSettings';
 import { LayoutItem } from './interfaces';
 
 function deepObjectMerge(original: any, newobj: any): object | LegendItemIcon | LayoutItem {
-  if (!newobj) return original;
+  if (!isObject(newobj)) return original;
   let returnedObj: {[index: string]: any} = copyObject(original);
-  if (isObject(newobj)) {
-    for (const key in newobj) {
-      if (isObject(newobj[key])) {
-        if (!returnedObj[key]) {
-          Object.assign(returnedObj, { [key]: newobj[key] });
-        } else {
-          returnedObj[key] = deepObjectMerge(returnedObj[key], newobj[key]);
-        }
-      } else {
+  for (const key in newobj) {
+    if (isObject(newobj[key])) {
+      if (!returnedObj[key]) {
         Object.assign(returnedObj, { [key]: newobj[key] });
+      } else {
+        returnedObj[key] = deepObjectMerge(returnedObj[key], newobj[key]);
       }
+    } else {
+      Object.assign(returnedObj, { [key]: newobj[key] });
     }
   }
   return returnedObj;
