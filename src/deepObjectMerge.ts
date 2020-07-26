6fcc250b68907fd2c31044acabcf7500e5deb8a7
Gavin Sykes <gavin@gavinsykes.uk>
2020-07-26 22:58:42 +0100

    Get deepObjectMerge working properly...I hope!

--- src/deepObjectMerge.ts
+++ src/deepObjectMerge.ts
@@ -1,22 +1,17 @@
 import isObject from './isObject';
+import copyObject from './copyObject';
 import { LegendItemIcon } from './defaultSettings';
 import { LayoutItem } from './interfaces';
 
-function copyObject(original: any): object {
-  let returnedObject = {};
-  Object.entries(original).map(c => returnedObject[c[0]] = isObject(c[1]) ? copyObject(c[1]) : c[1]);
-  return returnedObject;
-}
-
-function deepObjectMerge (original: any, ...newobjs: any[]): object | LegendItemIcon | LayoutItem {
+function deepObjectMerge(original: any, ...newobjs: any[]): object | LegendItemIcon | LayoutItem {
   if (!newobjs.length) return original;
-  let returnedObj = copyObject(original);
-  const newobj: any = newobjs.shift();
-  if (isObject(returnedObj) && isObject(newobj)) {
+  let returnedObj: {[index: string]: any} = copyObject(original);
+  const newobj: {[index: string]: any} = newobjs.shift();
+  if (isObject(newobj)) {
     for (const key in newobj) {
       if (isObject(newobj[key])) {
         if (!returnedObj[key]) Object.assign(returnedObj, { [key]: {} });
-        deepObjectMerge(returnedObj[key], newobj[key]);
+        returnedObj[key] = deepObjectMerge(returnedObj[key], newobj[key]);
       } else {
         Object.assign(returnedObj, { [key]: newobj[key] });
       }
