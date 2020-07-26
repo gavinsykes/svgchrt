5628037e96860fbb25d0491d08e67af58442bde5
Gavin Sykes <gavin@gavinsykes.uk>
2020-07-26 22:55:58 +0100

    Separating out copyObject function

---
+++ src/copyObject.ts
@@ -0,0 +1,10 @@
+import isObject from './isObject';
+
+function copyObject(original: any): object {
+  if (!isObject(original)) return original;
+  let returnedObject: {[index: string]: any} = {};
+  Object.entries(original).map(c => returnedObject[c[0]] = isObject(c[1]) ? copyObject(c[1]) : c[1]);
+  return returnedObject;
+}
+
+export default copyObject;
