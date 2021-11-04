56c73d724859341594b5af906f0f694b5fae3fee
Gavin Sykes <gavin@gavinsykes.uk>
2021-11-04 16:57:16 +0000

    Removing non-null assertion

--- dev/src/svgWrapText.ts
+++ dev/src/svgWrapText.ts
@@ -27,10 +27,10 @@ function svgWrapText(item: SVGTextElement, width: number): void {
   const x = item.getAttribute('x');
   const y = item.getAttribute('y');
   const dyval: string = item.getAttribute('dy')
-    ? item.getAttribute('dy')!
+    ? item.getAttribute('dy') as string
     : '0';
-  const value: number = parseFloat(dyval.match(/[0-9.]/)![0]);
-  const units: string = dyval.match(/[A-Za-z%]+/)![0];
+  const value: number = parseFloat((dyval.match(/[0-9.]/) as RegExpMatchArray)[0]);
+  const units: string = (dyval.match(/[A-Za-z%]+/) as RegExpMatchArray)[0];
   const dy = {
     value: value,
     units: units
